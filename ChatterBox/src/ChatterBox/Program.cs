73b881f5bc663a47943dfa958a9ae12df4071b86
Blake Rollins <brollins90@gmail.com>
2015-07-08 09:49:46 -0600

    a3 done

--- ChatterBox/src/ChatterBox/Program.cs
+++ ChatterBox/src/ChatterBox/Program.cs
@@ -7,7 +7,15 @@
         public void Main(string[] args)
         {
             IChatterBox box = new ChatterBox();
-            Console.WriteLine(box.ProcessString("How are you?"));
+            bool running = true;
+            while (running)
+            {
+                var input = Console.ReadLine();
+                var processed = box.ProcessString(input);
+                Console.WriteLine(processed);
+                if (processed.Contains("Bye")) running = false;
+            }
+            //Console.WriteLine(box.ProcessString("How are you?"));
 
             // "Bye!"
 
