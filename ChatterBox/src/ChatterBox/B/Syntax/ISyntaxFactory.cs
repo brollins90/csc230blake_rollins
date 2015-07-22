0b3e4378874e71089c41119afeb1672c68f85064
Blake Rollins <brollins90@gmail.com>
2015-07-21 19:21:47 -0600

    aaaa

--- ChatterBox/src/ChatterBox/B/Syntax/ISyntaxFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿//namespace ChatterBox.B.Syntax
-//{
-//    using Parser;
-
-//    public interface ITerminalFactory
-//    {
-//        ParserNode CreateNode(string input, params ParserNode[] children);
-//    }
-//}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Dot> dots = new List<Dot>();
+            dots.Add(new Dot { Group = 1, True = true, Type = 'X' });
+            dots.Add(new Dot { Group = 1, True = false, Type = 'Y' });
+            dots.Add(new Dot { Group = 1, True = true, Type = 'Z' });
+            dots.Add(new Dot { Group = 2, True = false, Type = 'X' });
+            dots.Add(new Dot { Group = 2, True = true, Type = 'Y' });
+            dots.Add(new Dot { Group = 2, True = false, Type = 'Z' });
+            dots.Add(new Dot { Group = 3, True = false, Type = 'X' });
+            dots.Add(new Dot { Group = 3, True = false, Type = 'Y' });
+            dots.Add(new Dot { Group = 3, True = true, Type = 'Z' });
+
+
+            foreach (var dot1 in dots)
+            {
+                foreach (var dot2 in dots)
+                {
+                    if ((dot1.Group != dot2.Group))
+                    {
+                        if ((dot1.Type != dot2.Type) || ((dot1.Type == dot2.Type) && (dot1.True == dot2.True)))
+                        {
+                            dot1.Connections.Add(dot2);
+                        }
+                    }
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    class Dot
+    {
+        public int Group { get; set; }
+        public bool True { get; set; }
+        public char Type { get; set; }
+        public List<Dot> Connections { get; set; } = new List<Dot>();
+
+        public override string ToString() => $"{Group} {Type}";
+    }
+}
